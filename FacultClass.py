d1a7dfb2ca30ce9fd3a0c1733266948e2ff01a1b
Milkyyyyyyy <78270404+Milkyyyyyyy@users.noreply.github.com>
2025-09-25 00:06:25 +0400

    Add FacultClass.py for facult class

---
+++ FacultClass.py
@@ -0,0 +1,14 @@
+import dbProcessing
+class Facult:
+    def __init__(self, facult_id):
+        facult_tuple = dbProcessing.getFacult(facult_id)
+        if facult_tuple is None:
+            print("No such facult")
+            self.id = -1
+            self.name = ""
+            return
+        self.id = facult_id
+        self.name = dbProcessing.getFacult(facult_id)[0]
+    # Returns name of
+    def getName(self):
+        return self.name
--- dbProcessing.py
+++ dbProcessing.py
@@ -1,23 +1,7 @@
 import sqlite3
+import FacultClass
 
 from main import facultObject
-
-
-# --------- Classes ---------
-# Returns facult by facult_id
-class Facult:
-    def __init__(self, facult_id):
-        facult_tuple = getFacult(facult_id)
-        if facult_tuple is None:
-            print("No such facult")
-            self.id = -1
-            self.name = ""
-            return
-        self.id = facult_id
-        self.name = getFacult(facult_id)[0]
-    def getName(self):
-        return self.name
-
 
 # --------- Methods ---------
 # Returns tuple of information from table about facult
@@ -34,8 +18,8 @@ def getFacult(facult_id):
         database.close()
         return None
 def getFacultObject(facult_id):
-    facultObject = Facult(facult_id)
-    if facultObject.id != -1:
-        return facultObject
+    facult_object = FacultClass.Facult(facult_id)
+    if facult_object.id != -1:
+        return facult_object
     else:
         return None
