3048ce4e2f83cc38d3e23e1b1e2f1ef7130c866c
Milkyyyyyyy <78270404+Milkyyyyyyy@users.noreply.github.com>
2025-09-24 22:40:46 +0400

    Датабазы + первые функции
    Добавил датабазу для проекта
    Добавил функцию getFacult(), которая возвращает факультет, соответствующий его ID

--- main.py
+++ main.py
@@ -1,1 +1,71 @@
-print("Test")
+import sqlite3
+
+# # Create conspects table
+# cursor.execute("""CREATE TABLE IF NOT EXISTS conspects(
+#     subject_id INTEGER,
+#     upload_date TEXT,
+#     conspect_date TEXT,
+#     user_telegram_id TEXT,
+#     theme TEXT,
+#     keywords TEXT,
+#     views INTEGER,
+#     status TEXT,
+#     rating INTEGER,
+#     anonymous INTEGER
+# )""")
+#
+# # Create conspects_files table
+# cursor.execute("""CREATE TABLE IF NOT EXISTS conspects_files(
+#     conspect_id INTEGER,
+#     path TEXT
+# )""")
+#
+# # Create users table
+# cursor.execute("""CREATE TABLE IF NOT EXISTS users(
+#     telegram_id TEXT,
+#     name TEXT,
+#     surname TEXT,
+#     study_group TEXT,
+#     direction_id INTEGER,
+#     role STRING
+# )""")
+#
+# # Create reactions table
+# cursor.execute("""CREATE TABLE IF NOT EXISTS reactions(
+#     conspect_id INTEGER,
+#     user_telegram_id TEXT,
+#     reaction INTEGER
+# )""")
+#
+# # Create facult table
+# cursor.execute("""CREATE TABLE IF NOT EXISTS facult(
+#     name TEXT
+# )""")
+#
+# # Create chair table
+# cursor.execute("""CREATE TABLE IF NOT EXISTS chair(
+#     facult_id INTEGER,
+#     name TEXT
+# )""")
+#
+# # Create direction table
+# cursor.execute("""CREATE TABLE IF NOT EXISTS directoin(
+#     chair_id INTEGER,
+#     name TEXT
+# )""")
+#
+# # Create direction table
+# cursor.execute("""CREATE TABLE IF NOT EXISTS subject(
+#     direction_id INTEGER,
+#     name TEXT
+# )""")
+# cursor.execute("INSERT INTO facults VALUES('ТЕСТ ФАКУЛЬТЕТ 2')")
+
+# test functions for database
+def getFacult(facult_id):
+    database = sqlite3.connect('conspects.db')
+    cursor = database.cursor()
+    cursor.execute(f"SELECT * FROM facults WHERE rowid={facult_id}")
+    output = cursor.fetchone()
+    database.close()
+    return output
