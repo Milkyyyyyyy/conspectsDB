f9fd3c2d9152150f30d0458fe3286b55d5d67bd9
Milkyyyyyyy <78270404+Milkyyyyyyy@users.noreply.github.com>
2025-09-24 22:46:57 +0400

    Комментарии

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sqlite3
 
+# Returns facult by facult_id
 def getFacult(facult_id):
     database = sqlite3.connect('conspects.db')
     cursor = database.cursor()
-    cursor.execute(f"SELECT * FROM facult WHERE rowid = {facult_id}")
+    cursor.execute(f"SELECT * FROM facults WHERE rowid = {facult_id}")
     output = cursor.fetchone()
     database.close()
     return output
-print(getFacult(2))
+print(getFacult(1))
