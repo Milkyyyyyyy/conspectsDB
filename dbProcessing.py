b76217259471e983959d0c90c65cf964b3397b73
Milkyyyyyyy <78270404+Milkyyyyyyy@users.noreply.github.com>
2025-09-24 23:26:13 +0400

    бебебе

---
+++ dbProcessing.py
@@ -0,0 +1,32 @@
+import sqlite3
+# --------- Classes ---------
+# Returns facult by facult_id
+class Facult:
+    def __init__(self, facult_id):
+        facult_tuple = getFacult(facult_id)
+        if facult_tuple is None:
+            print("No such facult")
+            self.id = -1
+            self.name = ""
+            return
+        self.id = facult_id
+        self.name = getFacult(facult_id)[0]
+    def getName(self):
+        return self.name
+
+
+# --------- Methods ---------
+# Returns tuple of information from table about facult
+# On errors returns None
+def getFacult(facult_id):
+    database = sqlite3.connect('conspects.db')
+    try:
+        cursor = database.cursor()
+        cursor.execute(f"SELECT * FROM facults WHERE rowid = {facult_id}")
+        output = cursor.fetchone()
+        database.close()
+        return output
+    except:
+        database.close()
+        return None
+
--- main.py
+++ main.py
@@ -1,35 +1,4 @@
-import sqlite3
+import dbProcessing
 
-# Returns facult by facult_id
-
-class Facult:
-    # Returns tuple of information from table about facult
-    # On errors returns None
-
-    def __init__(self, facult_id):
-        facult_tuple = getFacult(facult_id)
-        if facult_tuple is None:
-            print("No such facult")
-            self.id = -1
-            self.name = ""
-            return
-        self.id = facult_id
-        self.name = getFacult(facult_id)[0]
-    def getName(self):
-        return self.name
-
-def getFacult(facult_id):
-    database = sqlite3.connect('conspects.db')
-    try:
-        cursor = database.cursor()
-        cursor.execute(f"SELECT * FROM facults WHERE rowid = {facult_id}")
-        output = cursor.fetchone()
-        database.close()
-        return output
-    except:
-        database.close()
-        return None
-
-
-facultObject = Facult(2)
+facultObject = dbProcessing.Facult(1)
 print(facultObject.getName())
