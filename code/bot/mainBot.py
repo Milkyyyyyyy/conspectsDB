cea6278147f3197d20ac7daaba5115448eabe47b
Milkyyyyyyy <78270404+Milkyyyyyyy@users.noreply.github.com>
2025-10-09 00:07:43 +0400

    Создал проверку, зарегестрирован ли пользователь
    Пока что не знаю, работает ли вообще

--- code/bot/mainBot.py
+++ code/bot/mainBot.py
@@ -1,13 +1,40 @@
 import asyncio
-
+from code.database.repo.queries import connectDB, isExists
 from telebot.async_telebot import AsyncTeleBot
 
 API_TOKEN: str = '7428239168:AAHTx5DadgqK4Q7oKmhlBJkwAW9iRYHcaso'
 bot = AsyncTeleBot(API_TOKEN)
 
-@bot.message_handler(commands=['help', 'start'])
+@bot.message_handler(commands=['start'])
 async def send_welcome(message):
-    text = 'TEST BOT ANSWER'
-    await bot.reply_to(message, text)
+    userID= str(message.from_user.id)
+    database = connectDB()
+    isUserExists, _ = isExists(database=database, table="USERS", filters={"telegram_id": userID})
+    database.close()
+    print(isUserExists)
+    if not isUserExists:
+        text = 'Похоже, что вы не зарегистрированы. Если хотите пройти регистрацию вызовите команду `register`'
+        await bot.reply_to(message, text)
 
-asyncio.run(bot.polling())
+@bot.message_handler(commands=['register'])
+async def registerUser(message):
+    chatID = message.chat.id
+    userID= str(message.from_user.id)
+    database=connectDB()
+    isUserExists, _ = isExists(database=database, table="USERS", filters={"telegram_id": userID})
+    if isUserExists:
+        await bot.send("Похоже, вы уже зарегистрированы")
+        return
+    else:
+        await bot.send_message(chatID, "Запускаю процесс регистрации...")
+        # name = bot.register_next_step_handler(message, registerUser)
+        # await bot.send_message(chatID, name)
+    database.close()
+
+# async def getName(message):
+#     chatID = message.chat.id
+#     await bot.send_message(chatID, "Введи своё имя")
+#     userName = message.text
+#     await bot.send_message(chatID, f"Приятно познакомиться, {userName}")
+#     return userName
+# asyncio.run(bot.infinity_polling())
--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from code.logging import logger
-from code.database.repo.queries import connectDB, Tables, get, getAll, isExists, insert, remove, removeList
-from code.database.classes.namespaced import *
-# from code.bot import mainBot
+from code.bot import mainBot
 
-logger.info("Starts application...")
-database = connectDB()
-allRows = getAll(database=database, table="CHAIRS")
-print(allRows)
-
-response, cursor = get(database=database, table="CHAIRS", filters = {"rowid": 2})
-ns = getRowNamespaces(cursor, response)
-database.close()
+# logger.info("Starts application...")
+# database = connectDB()
+# allRows = getAll(database=database, table="CHAIRS")
+# print(allRows)
+#
+# response, cursor = get(database=database, table="CHAIRS", filters = {"rowid": 2})
+# ns = getRowNamespaces(cursor, response)
+# database.close()
