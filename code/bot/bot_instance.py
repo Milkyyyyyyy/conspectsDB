d0b9e24349867fb4eeaf61be20d4619f06f12574
Milkyyyyyyy <78270404+Milkyyyyyyy@users.noreply.github.com>
2025-10-29 22:39:06 +0400

    Исправил недостающий import

--- code/bot/bot_instance.py
+++ code/bot/bot_instance.py
@@ -9,6 +9,7 @@ from telebot.asyncio_storage import StateMemoryStorage
 from telebot.states.asyncio.middleware import StateMiddleware
 from telebot import asyncio_filters
 from code.bot.config import TOKEN
+from code.logging import logger
 
 bot = AsyncTeleBot(TOKEN, state_storage=StateMemoryStorage())
 bot.add_custom_filter(asyncio_filters.StateFilter(bot))
