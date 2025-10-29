33c8765b0d86c3e0ed1d4a6e748f2ebccfab45fd
Milkyyyyyyy <78270404+Milkyyyyyyy@users.noreply.github.com>
2025-10-29 22:27:06 +0400

    Добавил логи в main_menu.py
    Signed-off-by: Milkyyyyyyy <78270404+Milkyyyyyyy@users.noreply.github.com>

--- code/logging.py
+++ code/logging.py
@@ -5,5 +5,6 @@ logging.basicConfig(
 	filename='logs/app.log',
 	level=logging.DEBUG,
 	format='[%(asctime)s] - %(levelname)s %(funcName)s:%(lineno)d -- %(message)s',
+
 	datefmt='%Y-%m-%d %H:%M:%S'
 )
