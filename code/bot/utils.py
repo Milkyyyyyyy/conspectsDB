9c9420816fd7ff69c5ffbe35224e51b4faf10859
Milkyyyyyyy <brosuper500@gmail.com>
2025-11-25 21:31:05 +0400

    Добавляет функцию для отправки сообщения с файлами

--- code/bot/utils.py
+++ code/bot/utils.py
@@ -9,6 +9,8 @@ from zoneinfo import ZoneInfo
 
 from code.bot.bot_instance import bot
 from code.logging import logger
+import os
+from telebot import types
 
 
 async def delete_message_after_delay_interrupt(chat_id, message_id, delay_seconds=10):
@@ -87,6 +89,58 @@ async def safe_edit_message(
 	except:
 		logger.error("Unexpected error")
 		return
+
+async def send_message_with_files(
+		chat_id,
+		file_paths,
+		text,
+		reply_markup=None
+):
+	if not file_paths:
+		await bot.send_message(chat_id, text, reply_markup)
+		return
+
+	if isinstance(file_paths, str):
+		file_paths = [file_paths, ]
+
+	image_extensions = ('.jpg', '.jpeg', '.png', '.gif', '.webp')
+
+	photos = []
+	documents = []
+
+	for path in file_paths:
+		if not os.path.exists(path):
+			logger.warning(f'File {path} does not exist')
+			continue
+
+		if path.lower().endswith(image_extensions):
+			photos.append(path)
+		else:
+			documents.append(path)
+
+	total_files = len(photos) + len(documents)
+	if total_files > 1:
+		media_group = []
+		for i, photo_path in enumerate(photos):
+			with open(photo_path, 'rb') as photo:
+				caption = text if i == 0 else None
+				media_group.append(types.InputMediaPhoto(photo.read(), caption=caption))
+		for i, doc_path in enumerate(documents):
+			with open(doc_path, 'rb') as document:
+				caption = text if len(photos) == 0 and i == 0 else None
+				media_group.append(types.InputMediaDocument(document.read(), caption=caption))
+		message = await bot.send_mesage_group(chat_id, media_group)
+		if reply_markup:
+			await bot.edit_message_reply_markup(chat_id, message.id, reply_markup)
+
+	elif len(photos) == 1 and len(documents) == 0:
+		with open(photos[0], 'rb') as photo:
+			await bot.send_photo(chat_id, photo, caption=text, reply_markup=reply_markup)
+	elif len(documents) == 1 and len(photos) == 0:
+		with open(documents[0], 'rb') as document:
+			await bot.send_document(chat_id, document, caption=text, reply_markup=reply_markup)
+	
+
 
 
 async def get_greeting():
