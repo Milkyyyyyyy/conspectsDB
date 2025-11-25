cf45de9e6e87a271dc6dbfe10d02bb9158a4cf21
Milkyyyyyyy <brosuper500@gmail.com>
2025-11-26 00:02:33 +0400

    Обновляет логику контекстного менеджера
    Теперь можно использовать уже существующее подключение, передавая его в connect_db( v )
    
    Это позволяет не переподключаться каждый раз к датабазе, а вести одно подключение, пока оно нужно

--- code/database/service.py
+++ code/database/service.py
@@ -4,25 +4,35 @@ from code.database.config import CONSPECTS_DB
 from code.logging import logger
 # Контекстный менеджер для асинхронного подключения к датабазе
 class AsyncDBConnection:
-	def __init__(self, db_path):
-		self.db_path = db_path
-		self.conn = None
+	def __init__(self, db_or_conn):
+		if isinstance(db_or_conn, aiosqlite.Connection):
+			self.conn = db_or_conn
+			self.db_path = None
+			self.owns_connection = False
+		else:
+			self.db_path = db_or_conn
+			self.conn = None
+			self.owns_connection = True
 
 	async def __aenter__(self):
-		self.conn = await aiosqlite.connect(self.db_path)
-		self.conn.row_factory = aiosqlite.Row
+		if self.owns_connection:
+			self.conn = await aiosqlite.connect(self.db_path)
+			self.conn.row_factory = aiosqlite.Row
 		return self.conn
 
 	async def __aexit__(self, exc_type, exc_val, exc_tb):
-		if exc_type is None:
-			await self.conn.commit()
-		else:
-			await self.conn.rollback()
-		await self.conn.close()
+		if self.owns_connection:
+			if exc_type is None:
+				await self.conn.commit()
+			else:
+				await self.conn.rollback()
+			await self.conn.close()
 
 
-def connect_db():
+def connect_db(db_or_connection=None):
 	logger.debug('Async connecting to database...')
-	output = AsyncDBConnection(CONSPECTS_DB)
+	if db_or_connection is None or not isinstance(db_or_connection, aiosqlite.Connection):
+		db_or_connection = CONSPECTS_DB
+	output = AsyncDBConnection(db_or_connection)
 	logger.debug(output)
 	return output
